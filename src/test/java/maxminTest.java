8092c313943454c75f91df117dbf4bf17513b71c
ZhekaLited <94718651+ZhekaLited@users.noreply.github.com>
2022-04-18 21:57:40 +0300

    Version 0.1

---
+++ src/test/java/maxminTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class maxminTest {
+
+    @Test
+    public void findmax()  {
+        List<Integer> numbers = Arrays.asList(25, 14, 56, 15, 36, 56, 77, 18, 29, 49);
+
+        int max = maxmin.findMax(numbers);
+
+        assertThat(max).isEqualTo(14);
+    }
+//Write a Java program to find the maximum and minimum value of an array. Exercise 10
+}
